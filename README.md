cff29e82c7475e3534f1aff5b4285e62ffbdb7fb
Jaffeth Sosa <jaffeth_sosa@hotmail.com>
2024-11-30 15:20:16 -0600

    start proyect

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_crud_visitante
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_crud_visitante/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_crud_visitante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
